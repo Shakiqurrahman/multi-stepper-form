1a7e0251ccd5cb40f4e39c87e372269416b5aa94
Shakiqurrahman <rahmanshakiqur@gmail.com>
2025-04-10 01:55:54 +0600

    validation updated

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -7,9 +7,14 @@ export const FormDataSchema = z
       .string()
       .min(1, "Email is required")
       .email("Invalid email address"),
-    phoneNumber: z.string().min(10, "Phone number must be at least 10 digits"),
+    phoneNumber: z
+      .string({ required_error: "Phone number is required." })
+      .min(10, "Phone number must be at least 10 digits"),
     streetAddress: z.string().min(1, "Street address is required"),
     city: z.string().min(1, "City is required"),
+
+    // zod doesn't exist or work properly on the minLength or min char in number, that's why i am using string here
+
     zipCode: z.string().min(5, "Zip code must be at least 5 digits"),
     username: z.string().min(4, "Username must be at least 4 characters"),
     password: z.string().min(6, "Password must be at least 6 characters"),
